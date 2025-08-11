6b069a5a6635eb702dfc288a4e8751b6add7320e
Lance <miguel2002622@gmail.com>
2025-08-11 18:01:04 +0800

    Refactor main window and implement product display
    - Relocate MainWindow.xaml to Views/ and update project file
    - Add Product.cs to Models folder
    - Add MainViewModel to ViewModels folder
    - Create MainWindowViewModel.cs with sample product entries
    - Add DataGrid to MainWindow.xaml for product display

---
+++ RetailStoreInventory.Desktop/Views/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using RetailStoreInventory.Desktop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RetailStoreInventory.Desktop.Views
+{
+    /// <summary>
+    /// Code-behind for MainWindow.xaml.
+    /// Sets the DataContext to an instance of MainWindowViewModel to enable data binding.
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Bind the ViewModel to the View
+            DataContext = new MainWindowViewModel();
+        }
+    }
+}
